1511099248356716332ad6631295e32d5b12c2c8
Moenul <moenul22@gmail.com>
2023-08-12 11:52:20 +0600

    Nav Bar And Scroll to top Working Perfectly

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,33 @@
 $(document).ready(function(){
 
-//  Scroll to top button -------------------
+//  Nav Animate by Scroll and Scroll to top button -------------------
     $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $(".nav_bar").addClass("nav_bar_active");
+        } 
+        else {
+            $(".nav_bar").removeClass("nav_bar_active");
+        }
+
         if ($(this).scrollTop()) {
             $('.scroll_to_top').fadeIn();
         } else {
             $('.scroll_to_top').fadeOut();
         }
+
     });
 
     $(".scroll_to_top").click(function() {
-        $("html, body").animate({scrollTop: 0}, 500);
-     });
+        $("html, body").animate({scrollTop: 0}, 300);
+    });
+//  Nav Animate by Scroll and Scroll to top button -------------------
 
 
-    $(".nav_button").click(function() {
-        $(this).toggleClass('nav_button_active');
-        $(".nav_list").toggleClass('nav_list_active');
-     });
+
+    // $(".nav_button").click(function() {
+    //     $(this).toggleClass('nav_button_active');
+    //     $(".nav_list").toggleClass('nav_list_active');
+    // });
 
 
 
